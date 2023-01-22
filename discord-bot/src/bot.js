e490748f8a412046b41e0bdc43135ef125b7e1e7
crrystalz <lichpatnaik@gmail.com>
2023-01-22 10:06:55 -0800

    dikord boot time

--- discord-bot/src/bot.js
+++ discord-bot/src/bot.js
@@ -1,20 +1,32 @@
-require('dotenv').config()
+const {
+  Client,
+  Intents,
+  Collection
+} = require('discord.js');
+const fs = require('fs');
 
-const { token } = process.env
-const { Client, Collection, GatewayIntentBits, Intents } = require('discord.js')
-const fs = require('fs')
+const client = new Client({
+  intents: [
+      Intents.FLAGS.GUILDS,
+      Intents.FLAGS.GUILD_MESSAGES,
+      Intents.FLAGS.GUILD_MEMBERS,
+  ]
+})
 
-const client = new Client({ Intents: GatewayIntentBits.guilds })
-client.commands = new Collection()
-client.commandArray = []
+client.commands = new Collection();
 
-const functionFolders = fs.readdirSync('./src/functions')
-for (const folder of functionFolders) {
-  const functionFiles = fs.readdirSync('./src/functions/$(folder)').filter((file) => file.endsWith('.js'))
+require('dotenv').config();
 
-  for (const file of functionFiles) require('./functions/$(folder)')(client)
-}
+const functions = fs.readdirSync("./src/functions").filter(file => file.endsWith(".js"));
+const eventFiles = fs.readdirSync("./src/events").filter(file => file.endsWith(".js"));
+const commandFolders = fs.readdirSync("./src/commands").filter(file => fs.lstatSync(`./src/commands/${file}`).isDirectory());
 
-client.handleEvents()
-client.handleCommands()
-client.login(token)
+(async () => {
+  for (file of functions) {
+      require(`./functions/${file}`)(client);
+  }
+
+  client.handleEvents(eventFiles, "./src/events");
+  client.handleCommands(commandFolders, "./src/commands");
+  client.login(process.env.DISCORD_TOKEN);
+})();
--- discord-bot/src/events/client/interactionCreate.js
+++ discord-bot/src/events/client/interactionCreate.js
@@ -1,21 +1,20 @@
 module.exports = {
   name: 'interactionCreate',
   async execute(interaction, client) {
-    if (interaction.isChatInputCommand()) {
-      const { commands } = client
-      const { commandName } = interaction
-      const command = commands.get(commandName)
-      if (!command) return
+      if (!interaction.isCommand()) return;
+
+      const command = client.commands.get(interaction.commandName);
+
+      if (!command) return;
 
       try {
-        await command.execute(interaction, client)
+          await command.execute(interaction);
       } catch (error) {
-        console.error(error)
-        await interaction.reply({
-          content: 'Something went wrong while executing this comand...',
-          ephemoral: true
-        })
+          console.error(error);
+          await interaction.reply({
+              content: 'There was an error while executing this command!',
+              ephemeral: true
+          });
       }
-    }
   }
 }
